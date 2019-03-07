348208ac0cd22732f1724a26a3404460dc97ea02
Sol <solkim18@gmail.com>
2019-03-07 12:02:04 -0600

    completed FindPairs problem

--- FindPairs.java
+++ FindPairs.java
@@ -4,18 +4,26 @@
 */
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.ArrayList;
 
 public class FindPairs {
 	
 	/**
+	* O(nlogn)
 	* @param n the sum 2 of the array integers need to add up to
 	* @param array the integers to find pairs
 	*/
 	private static void findPairs(int sum, int[] array) {
 		Arrays.sort(array);
 		
-		ArrayList pairs = new ArrayList();
+		HashMap<Integer,Integer> pairs = new HashMap<Integer,Integer>();
+		
+		if (array.length<2) { //if there are less than 2 elements there are no pairs
+			System.out.println("Array too small! No Pairs!");
+			return;
+		}
 		
 		int leftPointer=0;
 		int rightPointer=array.length-1;
@@ -23,25 +31,40 @@ public class FindPairs {
 		while (leftPointer<rightPointer) {
 			int pairSum = array[leftPointer]+array[rightPointer];
 			
-			//System.out.println("pair sum: " + pairSum + " right pointer: " + rightPointer + " left pointer " + leftPointer);
 			if(pairSum>sum) {
 				rightPointer--;
-			} else if (pairSum==sum) {//save pair;
-				System.out.println("a pair! " + array[leftPointer] + "," + array[rightPointer]);
-				pairs.add(array[leftPointer],array[rightPointer]);
+			} else if (pairSum==sum) {//if you find a pair save it
+				if(!pairs.containsKey(leftPointer)) {
+					pairs.put(array[leftPointer],array[rightPointer]);
+				}
 				rightPointer--;
 			} else {//pairSum<Sum
 				leftPointer++;
 			}
-			for (int i=0; i<pairs.size(); i++) {
-				System.out.print(pairs.remove());
+		}
+		
+		List<Integer> keySet = new ArrayList<Integer>(pairs.keySet());
+		
+		if (keySet.size()==0) {
+			System.out.println("No pairs :(");
+		} else {
+			System.out.print("pairs: ");
+			for (int i=0; i<keySet.size(); i++) {
+				int firstInt=keySet.get(i);
+				System.out.print("(" + firstInt + "," + pairs.get(firstInt) + ") ");
 			}
+			System.out.println();
 		}
 	}
 	
 	public static void main(String[] args) {
 		int sum = 7;
-		int[] array = {5,2,4,6,4,9,0,1,3,4,7,8};
+		int[] array = {5,2,4,6,4,9,0,1,3,4,7,8,8,8,8,5,5};
+		//int[] array = {5,2};
+		//int[] array = {-1};
+		//int[] array = {5,3};
+		//int[] array = {5,3,3};
+		//int[] array = {};
 		findPairs(sum, array);
 	}
 }

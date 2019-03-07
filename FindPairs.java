e989211de8a9e099c878fd0a990bf03f84dbd19f
Sol <solkim18@gmail.com>
2019-03-06 22:03:37 -0600

    Initializing FindPairs problem

---
+++ FindPairs.java
@@ -0,0 +1,19 @@
+/**
+* How do you find all pairs of an integer array whose sum
+* is equal to a given number?
+*/
+
+public class FindPairs {
+	
+	/**
+	* @param n the sum 2 of the array integers need to add up to
+	* @param array the integers to find pairs
+	*/
+	private static void findPairs(int n, int[] array) {
+	
+	}
+	
+	public static void main(String[] args) {
+	
+	}
+}

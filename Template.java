ed2bfda1eaec47b315dc11f152bb3f6777faff19
Sol <solkim18@gmail.com>
2019-03-11 09:04:32 -0500

    created a template for problems Template.java

---
+++ Template.java
@@ -0,0 +1,27 @@
+/**
+* //Problem
+* 
+*
+* @version 1.0
+*/
+
+//imports
+
+public class Template {
+
+	/**
+	* //Time Complexity
+	*
+	* Summary:  
+	*			
+	* @param 
+	* @return 
+	*/
+	public static void function() {
+		//solution logic
+	}
+
+	public static void main(String[] args) {
+		//function call
+	}
+}

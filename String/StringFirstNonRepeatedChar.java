49bbe7aba678229725413587dae8c481cb16f1c9
Sol <solkim18@gmail.com>
2019-03-20 11:43:46 -0500

    completed StringFirstNonRepeatedChar

--- String/StringFirstNonRepeatedChar.java
+++ String/StringFirstNonRepeatedChar.java
@@ -4,7 +4,8 @@
 * @version 1.0
 */
 
-//imports
+import java.util.Map;
+import java.util.HashMap;
 
 public class StringFirstNonRepeatedChar {
 
@@ -16,11 +17,38 @@ public class StringFirstNonRepeatedChar {
 	* @param 
 	* @return 
 	*/
-	public static void function() {
-		//solution logic
+	public static void printFirstNonRepeatedChar(String string) {
+		Map<String, Integer> map = new HashMap<>();
+		
+		if(string.equals("")) {
+			System.out.println("It's blank! :O No characters to repeat :/");
+			return;
+		}
+		
+		String[] splitString = string.split(""); //split string into array
+		
+		for (String s : splitString) { //add all string chars to hashmap
+			if (map.containsKey(s)) map.replace(s,map.get(s)+1);
+			else map.put(s,1);
+		}
+		System.out.println(map);
+		
+		for (String findDup : splitString) {
+			if(map.get(findDup)==1) {
+				System.out.println("YOU FOUND THE FIRST NON-REPEATED CHARACTER FROM '" + string + "': " + findDup);
+				return;
+			}
+		}
+		System.out.println("No repeated characters here :'(");
+		return;
 	}
 
 	public static void main(String[] args) {
-		//function call
+		//String string = "oooowooo";
+		String string = "doodoodoop";
+		//String string = "";
+		//String string = "turnip";
+		
+		printFirstNonRepeatedChar(string);
 	}
 }

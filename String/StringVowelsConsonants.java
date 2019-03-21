0e9e6533f95be7e8ab294f579b443aee3f5c00d0
Sol <solkim18@gmail.com>
2019-03-21 13:47:23 -0500

    found more space efficient solution for StringVowelConsonants by just checking vowel char directly instead of using hashmap

--- String/StringVowelsConsonants.java
+++ String/StringVowelsConsonants.java
@@ -1,7 +1,7 @@
 /**
 * How do you count a number of vowels and consonants in a given string?
 *
-* @version 1.0
+* @version 2.0
 */
 
 import java.lang.Character;
@@ -11,12 +11,40 @@ import java.util.HashMap;
 public class StringVowelsConsonants {
 
 	/**
-	* O(n)
+	* O(n) - WITHOUT HASHMAP
+	*
+	* Summary:  iterate through the string and check if the character is a letter. if it is check if the character is a,e,i,o,u.
+	*			if so it is a vowel. if not it is a consonant  (ASSUMING Y IS NOT A VOWEL)
+	* @param 
+	*/
+	public static void countLetters(String string) {
+		int vowelCount=0;
+		int consonantCount=0;
+		
+		for (int i=0; i<string.length(); i++) {
+			char c = string.charAt(i);
+			if (Character.isLetter(c)) {
+				if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u') vowelCount++;
+				else consonantCount++;
+			}
+		}
+		System.out.println("Consonants: " + consonantCount + " Vowels: " + vowelCount);
+	}
+
+	public static void main(String[] args) {
+		//String string="";
+		//String string="11294kjjo;ihoih";
+		String string="i like trains";
+		
+		countLetters(string);
+	}
+
+	/**
+	* O(n) - WITH HASHMAP
 	*
 	* Summary:  create a hashmap of vowels, iterate through the string and check if the character is a letter. if it is check if the character in the
 	*			hashmap. if so it is a vowel. if not it is a consonant 
 	* @param 
-	*/
 	public static void countLetters(String string) {
 		int vowelCount=0;
 		int consonantCount=0;
@@ -38,12 +66,5 @@ public class StringVowelsConsonants {
 		}
 		System.out.println("Consonants: " + consonantCount + " Vowels: " + vowelCount);
 	}
-
-	public static void main(String[] args) {
-		//String string="";
-		//String string="11294kjjo;ihoih";
-		String string="i like trains";
-		
-		countLetters(string);
-	}
+	*/
 }

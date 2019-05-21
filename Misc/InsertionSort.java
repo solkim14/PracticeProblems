039b67bdcf3ae9f84a7958da830ca6d89e7ff87d
Sol <solkim18@gmail.com>
2019-05-21 09:22:24 -0500

    completed InsertionSort problem

--- Misc/InsertionSort.java
+++ Misc/InsertionSort.java
@@ -4,21 +4,29 @@
 * @version 1.0
 */
 
-//imports
-
 public class InsertionSort {
 
 	/**
 	* O(n^2)
 	*
-	* Summary:  Iterates array and puts each individual element in its ongoing correct place. Inefficient, but good
-	*			for mostly sorted arrays
+	* Summary:  Iterates array and puts each individual element in its ongoing correct place by comparing
+	*			to the previous element and swapping. Inefficient, but good for mostly sorted arrays
 	*			
 	* @param array array to be sorted
 	*/
 	public static void sort(int[] array) {
-		//solution logic
+		if (array.length<=1) return; //already sorted
+		for (int i=0; i<array.length; i++) {
+			int j=i;
+			int temp = array[i];
+			while (j>0 && temp<array[j-1]) {
+				array[j] = array[j-1];
+				j--;
+			}	
+			array[j] = temp;
+		}
 	}
+
 
 	/** helper function - prints given array*/
 	public static void printArray(int[] array) {
@@ -30,8 +38,16 @@ public class InsertionSort {
 	}
 	
 	public static void main(String[] args) {
-		int[] array = {};
-		
+		//int[] array = {};
+		//int[] array = {1};
+		//int[] array = {2,1};
+		//int[] array = {1,2};
+		//int[] array = {2,3,1};
+		//int[] array = {1,2,3};
+		int[] array = {4,0,9,7,2,2,0,7};
+		//int[] array = {1,0,0,8,7,3,0,9,8,7,6,0,3,7,5,0,9,2,3,0,1};
+		printArray(array);
 		sort(array);
+		printArray(array);
 	}
 }
